30351f31cd07c16ed81734b772e0768f38b08fd9
Gilbert Schwaab <gilbert@convey.de>
2023-10-04 11:38:12 +0200

    Update xhrRequest.js

--- xhrRequest/xhrRequest.js
+++ xhrRequest/xhrRequest.js
@@ -7,23 +7,18 @@
 
 (function () {
     "use strict";
-    var subscriptionKey = "a12ee952460d409f9f66d1536dd97318";
-    var sourceImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Atomist_quote_from_Democritus.png/338px-Atomist_quote_from_Democritus.png";
-    var uriBase = "https://westeurope.api.cognitive.microsoft.com/vision/v1.0/ocr?language=de";
-
-    var imageRecord = {
-        url: sourceImageUrl
-    };
-
-    var options = {
-        type: "POST",
-        url: uriBase,
-        data: JSON.stringify(imageRecord),
-        headers: {
-            "Content-Type": "application/json",
-            "Ocp-Apim-Subscription-Key": subscriptionKey
-        }
-    };
+    var urls = [
+        'https://leadsuccess.convey.de/LSModata_online/MitarbeiterVIEW_20431?$format=json',
+        'https://leadsuccess.convey.de/LS1Modata_online/MitarbeiterVIEW_20431?$format=json',
+        'https://leadsuccess.convey.de/LS2Modata_online/MitarbeiterVIEW_20431?$format=json',
+        'https://leadsuccess.convey.de/LS3Modata_online/MitarbeiterVIEW_20431?$format=json',
+        'https://leadsuccess.convey.de/LS4Modata_online/MitarbeiterVIEW_20431?$format=json'/*,
+        'https://leadsuccess.convey.de/LSNodata_online/MitarbeiterVIEW_20431?$format=json'*/
+    ];
+    var userList = [
+        { login: '', password: '' }
+    ];
+    var userCounter = Math.floor(Math.random() * userList.length);
 
     // Set up the request
     var dispatcher = {
@@ -32,62 +27,58 @@
             this.timestamp = null;
             this.successCount = 0;
             this.errorCount = 0;
-            this.waitTimeMs = 20000;
+            this.waitTimeMs = Math.floor(250 + Math.random() * 500);
+            this.lastLogin = "";
+            this.lastCount = 0;
+            this.duration = 0;
+            this.lastUrlIdx = 0
 
             Log.ret(Log.l.trace);
             return WinJS.Promise.as();
         },
 
         activity: function () {
-            Log.print(Log.l.info, "calling xhr method=POST url=" + sourceImageUrl);
-            var that = this;
-            return WinJS.xhr(options).then(function (response) {
-                var err;
-                Log.print(Log.l.trace, "success!");
-                try {
-                    var obj = response;
-                    console.log(obj);
-                    var myresultJson = JSON.parse(response.responseText);
-                    if (obj && obj.responseText) {
-                        that.successCount++;
-                        Log.print(Log.l.info, "select success! " + that.successCount + " success / " + that.errorCount + " errors");
-                        that.timestamp = new Date();
-                    } else {
-                        that.errorCount++;
-                        Log.print(Log.l.error, "select error! " + that.successCount + " success / " + that.errorCount + " errors");
-                        that.timestamp = new Date();
-                        err = { status: 404, statusText: "no data found" };
+            Log.call(Log.l.trace, "xhrRequest.");
+            userCounter++;
+            if (userCounter >= userList.length) {
+                userCounter = 0;
+            }
+            var user = userList[userCounter].login;
+            var password = userList[userCounter].password;
+            var idx = Math.floor(Math.random() * urls.length);
+            var url = urls[idx];
+            var options = {
+                type: "GET",
+                url: url,
+                user: user,
+                password: password,
+                customRequestInitializer: function (req) {
+                    if (typeof req.withCredentials !== "undefined") {
+                        req.withCredentials = true;
                     }
-                    var myResultSet = "";
-                    var dummyResult = "";
-                    if (myresultJson && myresultJson.regions.length > 0) {
-                        for (var i = 0; i < myresultJson.regions.length; i++) {
-                            for (var j = 0; j < myresultJson.regions[i].lines.length; j++) {
-                                for (var k = 0; k < myresultJson.regions[i].lines[j].words.length; k++) {
-                                    var myBoundingBox = myresultJson.regions[i].lines[j].words[k].boundingBox;
-                                    var myNewboundingBox = myBoundingBox.split(",");
-                                    myResultSet = myResultSet + "(x = " + myNewboundingBox[0] + ", y = " + myNewboundingBox[1] + ", width = " + myNewboundingBox[2] + ", height = " + myNewboundingBox[3] + ", text = "
-                                        + myresultJson.regions[i].lines[j].words[k].text + "); ";
-                                    var text = (myresultJson.regions[i].lines[j].words[k].text).replace(",", ".");
-                                    dummyResult = myNewboundingBox[0] + ", " + myNewboundingBox[1] + ", " + myNewboundingBox[2] + ", " + text + "\n";
-
-                                }
-                            }
-                        }
-                    }
-                    console.log(dummyResult);
-                } catch (exception) {
-                    that.errorCount++;
-                    Log.print(Log.l.error, "resource parse error " + (exception && exception.message) + that.successCount + " success / " + that.errorCount + " errors");
-                    that.timestamp = new Date();
-                    err = { status: 500, statusText: "data parse error " + (exception && exception.message) };
+                },
+                headers: {
+                    "Authorization": "Basic " + btoa(user + ":" + password)
                 }
-                return WinJS.Promise.as();
+            };
+            Log.print(Log.l.info, "calling xhr method=GET user[" + userCounter + "]=" + userList[userCounter].login);
+            var that = this;
+            var startTS = new Date();
+            var ret = WinJS.xhr(options).then(function (response) {
+                Log.print(Log.l.info, "select success! " + that.successCount + " success / " + that.errorCount + " errors");
+                that.lastCount++;
+                that.lastLogin = user;
+                var now = new Date();
+                that.duration = now.getTime() - startTS.getTime();
+                that.timestamp = now;
+                that.lastUrlIdx = idx;
             }, function (errorResponse) {
                 that.errorCount++;
-                Log.print(Log.l.error, "error status=" + that.errorResponse.status + " statusText=" + that.errorResponse.statusText);
+                Log.print(Log.l.error, "error status=" + errorResponse.status + " statusText=" + errorResponse.statusText);
                 that.timestamp = new Date();
             });
+            Log.ret(Log.l.trace);
+            return ret;
         },
 
         dispose: function () {
@@ -98,7 +89,7 @@
 
         info: function () {
             Log.call(Log.l.trace, "xhrRequest.");
-            var infoText = this.successCount + " success / " + this.errorCount + " errors";
+            var infoText = this.lastCount + " success / " + this.errorCount + " errors " + this.duration + "ms login " + this.lastLogin + " url-idx " + this.lastUrlIdx;
             if (this.timestamp) {
                 infoText += "\n" + this.timestamp.toLocaleTimeString();
             }
