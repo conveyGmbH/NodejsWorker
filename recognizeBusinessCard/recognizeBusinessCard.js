15a95e75d421400731b3df48f4b342934d542549
Gilbert Schwaab <gilbert@convey.de>
2023-10-17 21:06:36 +0200

    Update recognizeBusinessCard.js

--- recognizeBusinessCard/recognizeBusinessCard.js
+++ recognizeBusinessCard/recognizeBusinessCard.js
@@ -8,10 +8,10 @@
 (function () {
     "use strict";
 
-    //const { FormRecognizerClient, AzureKeyCredential } = require("@azure/ai-form-recognizer");
-    //const fs = require("fs");
+    const { DocumentAnalysisClient, AzureKeyCredential } = require("@azure/ai-form-recognizer");
+    const fs = require("fs");
 
-    //var fileName = "images/business_card.jpg";
+    var fileName = "images/business_card.jpg";
 
     // Set up the request
     var dispatcher = {
@@ -23,8 +23,8 @@
             this.waitTimeMs = 60000;
 
             // You will need to set these environment variables or edit the following values
-            //this.endpoint = "https://westeurope.api.cognitive.microsoft.com/";
-            //this.apiKey = "eb0abaaf63d3477c95c3f6f645be1eab";
+            this.endpoint = "https://westeurope.api.cognitive.microsoft.com/";
+            this.apiKey = "eb0abaaf63d3477c95c3f6f645be1eab";
 
             Log.ret(Log.l.trace);
             return WinJS.Promise.as();
@@ -33,26 +33,31 @@
         activity: function () {
             Log.call(Log.l.trace, "recognizeBusinessCard.");
 
-            /*var readStream = fs.createReadStream(fileName);
+            var readStream = fs.createReadStream(fileName);
 
-            var client = new FormRecognizerClient(this.endpoint, new AzureKeyCredential(this.apiKey));
+            var client = new DocumentAnalysisClient(this.endpoint, new AzureKeyCredential(this.apiKey));
 
-            var promise = client.beginRecognizeBusinessCards(readStream, {
-                contentType: "image/jpeg",
-                onProgress: (state) => {
-                    Log.print(Log.l.trace, "status: " + state.status);
-                }
-            }).then(function (poller) {
-                return poller.pollUntilDone();
-            }).then(function (businessCard) {
-                Log.print(Log.l.trace, businessCard.toString());
-            });*/
-
-            var winPromise = WinJS.Promise.as();//toWinJSPromise(promise);
-
-
+            var promise = new WinJS.Promise.timeout(0).then(function() {
+                client.beginAnalyzeDocument("prebuilt-businessCard", readStream).then(function (poller) {
+                    return poller.pollUntilDone();
+                }).then(function (result) {
+                    if (result && result.documents && result.documents[0]) {
+                        var fields = result.documents[0].fields;
+                        for (var fieldName in fields) {
+                            if (fields.hasOwnProperty(fieldName)) {
+                                var values = fields[fieldName] && fields[fieldName].values;
+                                if (values && values[0] && values[0].value) {
+                                    Log.print(Log.l.trace, fieldName + ": " + values[0].value.replace(/\n/g," ") + " (confidence: " + values[0].confidence + "%)");
+                                }
+                            }
+                        }
+                    }
+                    promise._completed();
+                });
+                return WinJS.Promise.timeout(30000);
+            });
             Log.ret(Log.l.trace);
-            return winPromise;
+            return promise;
         },
 
         dispose: function () {
@@ -63,7 +68,7 @@
 
         info: function () {
             Log.call(Log.l.trace, "recognizeBusinessCard.");
-            var infoText = this.lastCount + "recognizeBusinessCard: success / " + this.errorCount + " errors ";
+            var infoText = this.successCount + " success / " + this.errorCount + " errors ";
             Log.ret(Log.l.trace);
             return infoText;
         }
